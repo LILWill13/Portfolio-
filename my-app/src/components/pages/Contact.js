4bd26a2e579fd9d2efcdf63c48706443404b0b4f
noah gaston <noahsfuture18@gmail.com>
2022-05-22 12:20:42 -0400

    contact page componet

---
+++ my-app/src/components/pages/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <div id="list">
+                <ul>
+                    <li>954.669.3357</li>
+                    <li><a href = "mailto: Noahsfuture18@gmail.com">Noahsfuture18@gmail.com</a></li>
+                    <li><a target="_blank" rel="noreferrer" href="https://github.com/LILWill13">GitHub</a></li>
+                    <li><a target="_blank" rel="noreferrer" href="https://twitter.com/NoahW_Gaston">Twitter</a></li>
+                    <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/noah-gaston-54a30922b/">Linkedin</a></li>
+                </ul>
+            </div>
+    </div>
+  );
+}
