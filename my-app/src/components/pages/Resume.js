780097f7edda9d73d43a57f9691bf9030c26d3ae
noah gaston <noahsfuture18@gmail.com>
2022-05-22 12:19:35 -0400

    resume page componet

---
+++ my-app/src/components/pages/Resume.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <div>
+        <h2> NOAH GASTON </h2>
+        <p> Atlanta, GA 30144 | 1-954-669-3357 | Noahsfuture18@gmail.com </p>
+
+        <ul>
+          <li> Github - LilWill13 </li>
+          <li> Linkedin - https://www.linkedin.com/in/noah-gaston-54a30922b </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Professional Summary</h2>
+
+        <p>
+          Collaborative leader with dedication to partnering with coworkers to promote engaged, empowering work culture. Strengths in building and maintaining relationships with diverse range of stakeholders in dynamic, agile settings. 
+        </p> 
+      </div>
+
+      <div>
+        <h2>Skills</h2>
+        <ul> 
+          <li>Front-End Programming: HTML5, CSS3, JavaScript</li>
+          <li>Time Management</li>
+          <li>Teamwork and Collaboration</li>
+          <li>Avid Learner</li>
+          <li>Work History</li>
+          <li>Manager</li>
+          <li>Continuous Improvement</li>
+          <li>Version Control Systems: Git GitHub JavaScript/CSS Libraries and Frameworks </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Experience</h2>
+        <h3>Wing Zone</h3>
+        <ul> 
+          <li><h3>Manager</h3></li>
+          <li>Jun 2020 - Apr 2021</li>
+          <li>Kennesaw, GA</li>
+        </ul>
+        <p> 
+        Cross-trained existing employees to maximize team agility and performance.
+        Delivered feedback to decision-makers regarding employee performance and training needs. Maximized performance by monitoring daily activities and mentoring team members. Collaborated with team members to achieve goals 
+        </p>
+        <h3>The Varsity</h3>
+        <ul>
+          <li><h3>Team Member</h3></li>
+          <li>Apr 2021 - Mar 2022</li>
+          <li>Kennesaw, GA</li>
+        </ul>
+
+        <p>
+        Learned all required tasks quickly to maximize performance.
+        Developed strong cooperative relationships with coworkers and managers.
+        Pursued learning opportunities to advance knowledge and take on leadership position. Worked well with teammates and accepted coaching from management team.
+        </p>
+      </div>
+
+      <div>
+        <h2>Education</h2> 
+        <h3> North Cobb High School </h3>
+        <p> High School Diploma </p>
+      </div>
+      <div>
+        <h2> Certifications </h2>
+        <h3> Georgia Tech Full-Stack web-development training course </h3>
+      </div>
+    </div>
+  );
+}
