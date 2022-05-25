f1f9cc0c11e102c475773711de0305cdba16e16e
noah gaston <noahsfuture18@gmail.com>
2022-05-25 09:25:03 -0400

    projects images link and styling

--- my-app/src/components/Portfolio.js
+++ my-app/src/components/Portfolio.js
@@ -6,7 +6,7 @@ import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Resume');
+  const [currentPage, setCurrentPage] = useState('Projects');
 
   const renderPage = () => {
     if (currentPage === 'Resume') {
--- my-app/src/components/pages/Projects.js
+++ my-app/src/components/pages/Projects.js
@@ -1,17 +1,57 @@
 import React from 'react';
-
+import img1 from "../imgs/1.jpg"
+import img2 from "../imgs/2.jpg"
+import img3 from "../imgs/3.jpg"
+import img4 from "../imgs/4.jpg"
+import img5 from "../imgs/5.jpg"
 const styles = {
-  title:{
+  outterbody:{
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    flexwrap: "wrap"
+  },
 
+  innerBody: {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: "wrap",
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: "90vw"
+  },
+
+  imgs:{
+    width: "20rem",
+    height: "20rem",
+    border: "3px solid black",
+    margin: "15px",
+    boxShadow: '10px 10px  10px black',
   }
 }
 
 export default function Projects() {
   return (
-    <div>
-      <div style={styles.title}>
+    <div style={styles.outterbody}>
         <h1>Projects</h1>
-      </div>
+        <div style={styles.innerBody}>
+          <div>
+            <img src={img1} style={styles.imgs} alt=""></img>
+          </div>
+          <div>
+            <img src={img2} style={styles.imgs} alt=""></img>
+          </div>
+          <div>
+            <img src={img3} style={styles.imgs} alt=""></img>
+          </div>
+          <div>
+            <img src={img4} style={styles.imgs} alt=""></img>
+          </div>
+          <div>
+            <img src={img5} style={styles.imgs} alt=""></img>
+          </div>
+        </div>
     </div>
   );
 }
